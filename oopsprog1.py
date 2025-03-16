06ffe30acda515ad0fe57b923a2d7ae4dfa35aec
Imran-git-student <imran-git-email2rockyabc@gmail.com>
2025-03-17 02:40:56 +0530

    new

--- oopsprog1.py
+++ oopsprog1.py
@@ -4,9 +4,8 @@ from car import Car
 car1 = Car("Mustang", 2024, "red", False)
 car2 = Car("Toyota", 2023, "white", True)
 car3 = Car("maruthi", 2025, "white", True)
+
 print(car3.model)
 print(car3.year)
 print(car3.color)
 print(car3.for_sale)
-
-#car3.drive()
